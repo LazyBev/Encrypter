b532bcdb80bc9ef21654d2f121d679d261abb04a
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-08-03 02:02:16 +0000

    Update Enc.c

--- Enc.c
+++ Enc.c
@@ -3,52 +3,73 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/random.h>
+#include <string.h>
 
-int random_number(int min_num, int max_num)
-    {
-        int result = 0, low_num = 0, hi_num = 0;
+int random_number(int min_num, int max_num) {
+    int result = 0, low_num = 0, hi_num = 0;
 
-        if (min_num < max_num)
-        {
-            low_num = min_num;
-            hi_num = max_num + 1; // include max_num in output
-        } else {
-            low_num = max_num + 1; // include max_num in output
-            hi_num = min_num;
-        }
-
-        srand(time(NULL));
-        result = (rand() % (hi_num - low_num)) + low_num;
-        return result;
+    if (min_num < max_num) {
+        low_num = min_num;
+        hi_num = max_num + 1; // include max_num in output
+    } else {
+        low_num = max_num + 1; // include max_num in output
+        hi_num = min_num;
     }
+
+    srand(time(NULL));
+    result = (rand() % (hi_num - low_num)) + low_num;
+    return result;
+}
 
 int main(int argc, char *argv[]) {
-    FILE *in, *out;
-    int ch;
-    int ent = atoi(argv[3]);
-
     srand(time(NULL) + getpid());
 
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <input file> <output file> <integer>\n", argv[0]);
+    FILE *conf = fopen("config.txt", "r");
+    if (conf == NULL) {
+        perror("Failed to open config.txt");
         return 1;
     }
 
-    in = fopen(argv[1], "r");
+    char input_file[256];
+    char output_file[256];
+
+    if (fgets(input_file, sizeof(input_file), conf) == NULL) {
+        perror("Failed to read input file name from config.txt");
+        fclose(conf);
+        return 1;
+    }
+
+    if (fgets(output_file, sizeof(output_file), conf) == NULL) {
+        perror("Failed to read output file name from config.txt");
+        fclose(conf);
+        return 1;
+    }
+
+    // Remove newline character from the file names
+    input_file[strcspn(input_file, "\n")] = '\0';
+    output_file[strcspn(output_file, "\n")] = '\0';
+
+    fclose(conf);
+
+    FILE *in = fopen(input_file, "r");
     if (in == NULL) {
-        perror("Error opening input file");
+        perror("Failed to open input file");
         return 1;
     }
 
-    out = fopen(argv[2], "w");
+    FILE *out = fopen(output_file, "w");
     if (out == NULL) {
-        perror("Error opening output file");
+        perror("Failed to open output file");
         fclose(in);
         return 1;
     }
 
+    int ch;
+    int ent = (int)((rand() * (rand() / getpid())) * getpid() + (getpid() * getppid()));
+
     int i = 0;
     int randomiz = ((random_number(1, ent) * (getpid() % 2) * getpid()) + (++i + (ent - (i - (i / 2)))));
+
     while ((ch = fgetc(in)) != EOF) {
         fputc(ch + randomiz, out);
         randomiz = (i % getpid() == 1) ? ((random_number(1, ent)) * (getpid() % 2) + (++i + (ent - i))) + ++i : ((random_number(1, ent)) + (getpid() % 2) - (++i + (ent - (i * i)))) + --i;
